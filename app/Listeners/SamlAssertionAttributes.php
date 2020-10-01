45e5c3858014ae9a36b7c0434a86acaa027c1059
jayson-pamittan <jayson.pamittan@gmail.com>
2020-10-01 22:58:53 +0800

    Update SamlAssertionAttributes.

--- app/Listeners/SamlAssertionAttributes.php
+++ app/Listeners/SamlAssertionAttributes.php
@@ -29,11 +29,10 @@ class SamlAssertionAttributes
     public function handle($event)
     {
         $event->attribute_statement
-            ->addAttribute(new Attribute(ClaimTypes::service_level, "Prime8"))
-            ->addAttribute(new Attribute(ClaimTypes::policy_number, auth()->user()->policy_number))
-            ->addAttribute(new Attribute(ClaimTypes::client_name, auth()->user()->client_name))
-            ->addAttribute(new Attribute(ClaimTypes::first_name, auth()->user()->first_name))
-            ->addAttribute(new Attribute(ClaimTypes::last_name, auth()->user()->last_name))
-            ->addAttribute(new Attribute(ClaimTypes::email, auth()->user()->email));
+            ->addAttribute(new Attribute(ClaimTypes::SERVICE_LEVEL, "Prime8"))
+            ->addAttribute(new Attribute(ClaimTypes::POLICY_NUMBER, auth()->user()->policy_number))
+            ->addAttribute(new Attribute(ClaimTypes::CLIENT_NAME, auth()->user()->client_name))
+            ->addAttribute(new Attribute(ClaimTypes::FIRST_NAME, auth()->user()->first_name))
+            ->addAttribute(new Attribute(ClaimTypes::LAST_NAME, auth()->user()->last_name));
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Listeners\SamlAssertionAttributes;
+use CodeGreenCreative\SamlIdp\Events\Assertion;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,11 +20,8 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
-        ],
-        'CodeGreenCreative\SamlIdp\Events\Assertion' => [
-            'App\Listeners\SamlAssertionAttributes'
+        Assertion::class => [   
+            SamlAssertionAttributes::class
         ],
     ];
 
